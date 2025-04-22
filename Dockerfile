54fb932cd2f7bcb6ae9d90ae770bc3fde72b055a
Gabriel Moraes <gabrielmoraes@lance.com.br>
2025-04-22 14:09:07 -0300

    refactor: remove comments

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,5 @@
 FROM wordpress:php8.2-apache
 
-# Definir o idioma para o Português do Brasil
-ENV WORDPRESS_LANG pt_BR
-
-# Copia wp-content com plugins e mu-plugins
 COPY wp-content /var/www/html/wp-content
 
-# Copia wp-config.php
 COPY wp-config.php /var/www/html/wp-config.php
--- wp-config.php
+++ wp-config.php
@@ -1,7 +1,4 @@
 <?php
-define('WPLANG', 'pt_BR');
-
-// Banco de dados
 define('DB_NAME', getenv('WORDPRESS_DB_NAME') ?: 'wpdb');
 define('DB_USER', getenv('WORDPRESS_DB_USER') ?: 'wpuser');
 define('DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD') ?: 'wppassword');
@@ -9,7 +6,6 @@ define('DB_HOST', getenv('WORDPRESS_DB_HOST') ?: 'db');
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 
-// Chaves de segurança
 define('AUTH_KEY', 'd.j^J`PnBEt}qZjhGASTV2#Z|(oGCoLB[^#D~9XmMs<Sb9+s@$m8*R:{S9pc(Mn>');
 define('SECURE_AUTH_KEY', 'h~78K1I-Eqj{Ub^RFIM`T@#1^3# 3B72<lm7ptPYrpXs}|U-TdU54UVcP2&#28nt');
 define('LOGGED_IN_KEY', '-{QZ)VtH9Y(%EhrSb4iw=2``yc`&^^pzz`dwA0/FN<Lc=BC,c<S7-J{DZStr,h_v');
@@ -19,27 +15,23 @@ define('SECURE_AUTH_SALT', '|mVQHkj;I;SCnBIf Q8%.#8G^@4SL(fF):GuJE]v+qi#?+XJ;5B-
 define('LOGGED_IN_SALT', 'HcW{RZNaEp,.8:In{2)]6Tspy!<{Gy-wmzgzzfl2(x[3,T`Nb]?,](:f~&d~(-4E');
 define('NONCE_SALT', 'w($+VGTsGS:sGcL+Y%7#u}r+#$Of^:aLQv>5up>zutz3}}E;qdv[1xh9JyNX/`tS');
 
-// Configuração WP Stateless para Google Cloud Storage
 define('WP_STATLESS_MEDIA', true);
 define('WP_STATELESS_MEDIA_MODE', 'stateless');
 define('WP_STATLESS_MEDIA_BUCKET', getenv('WP_STATLESS_GCS_BUCKET_NAME'));
 define('WP_STATLESS_MEDIA_KEY_FILE', getenv('WP_STATLESS_GCS_KEY_FILE'));
 define('WP_STATELESS_MEDIA_ROOT_DIR', 'wp-content/uploads');
-define('WP_STATLESS_MEDIA_CREATE_BUCKET', false); // Não cria o bucket automaticamente
+define('WP_STATLESS_MEDIA_CREATE_BUCKET', false);
 define('WP_STATLESS_MEDIA_CACHE_CONTROL', 'public,max-age=3600');
-define('WP_STATLESS_UPLOAD_LOCAL', true); // Permite upload local para miniaturas, se necessário
+define('WP_STATLESS_UPLOAD_LOCAL', true);
 
 $table_prefix = 'wp_';
 
-// Debugging
 define('WP_DEBUG', false);
 
-// Caminho do WordPress
 if (!defined('ABSPATH')) {
   define('ABSPATH', __DIR__ . '/');
 }
 
-// Configuração de HTTPS para quando o protocolo for 'https'
 if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
   $_SERVER['HTTPS'] = 'on';
 }
